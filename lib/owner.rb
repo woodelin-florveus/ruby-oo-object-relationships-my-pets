7759cdc499505987ad58b56fdc072916a3952554
woodelin-florveus <woodelinflorveus@gmail.com>
2020-12-10 17:43:01 -0500

    hey

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 
 class Owner
-  attr_accessor :owner, :say_species, :all
+  attr_accessor :owner, :say_species, :reset_all
   attr_reader :name, :species
 
   
@@ -11,7 +11,7 @@ class Owner
   
   def initialize(name)
     @name = name
-     @owner = owner
+    @owner = owner
     @species = "human"
     @say_species = "I am a human."
     @@all << self
@@ -22,7 +22,13 @@ class Owner
     @@all
   end
 
+  def self.count
+    # binding.pry
+     self.all.count
+  end
 
-
+  def self.reset_all
+    self.all.clear
+  end
 
 end
